e2230e253370586af0ce1594b63720f6ba550585
Ryan Evertz <ryanevertzz@gmail.com>
2022-11-22 22:49:05 +0100

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <tchar.h>
+#include <windows.h>
+//wsa version
+#define SCK_VERSION2 0x0202 //version 2
+#define DEFAULT_PORT 55555;
+
+//Save wsa information
+WSADATA DATA;
+SOCKET ServerSocket;
+SOCKADDR_IN ServerInfo;
+
+
+int main() {
+	//iniatilsing wsa
+	long SUCCESSFULL = WSAStartup(SCK_VERSION2, &DATA);
+	
+	if (SUCCESSFULL != 0) {
+		std::cout << "WSA Exited with error: " << SUCCESSFULL;
+	}
+	else {
+		std::cout << "WSA Initialised" << std::endl;
+	}
+
+	ServerSocket = INVALID_SOCKET;
+	ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ServerSocket == INVALID_SOCKET) {
+		std::cout << "Socket error: "<<WSAGetLastError()<<std::endl;
+		WSACleanup();
+		return EXIT_SUCCESS;
+	}
+	else {
+		std::cout << "Socket has been initialised" << std::endl;
+	}
+
+	ServerInfo.sin_family = AF_INET;
+	ServerInfo.sin_port = htons(55555);
+	InetPton(AF_INET, _T("127.0.0.1"), &ServerInfo.sin_addr.s_addr);
+
+	if (bind(ServerSocket, (SOCKADDR*)&ServerInfo, sizeof(ServerInfo))) {
+		std::cout << "Error binding: " << WSAGetLastError() << std::endl;
+		closesocket(ServerSocket);
+		WSACleanup();
+		return 0;
+	}
+	else {
+		std::cout << "Succefully bound socket" << std::endl;
+	}
+
+	if (listen(ServerSocket, 30) != 0) {
+		std::cout << "Error listening: " << WSAGetLastError() << std::endl;
+		closesocket(ServerSocket);
+		WSACleanup();
+		return 0;
+	}
+	else {
+		std::cout << "Succefully listening socket" << std::endl;
+	}
+
+	fd_set Master;
+	FD_ZERO(&Master);
+	FD_SET(ServerSocket, &Master);
+
+	while (true) {
+		fd_set copy = Master;
+
+
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+
+
+		for (int i = 0; i < socketCount; i++) {
+			SOCKET socket = copy.fd_array[i];
+			if (socket == ServerSocket) {
+				SOCKET client = accept(ServerSocket, nullptr, nullptr);
+				FD_SET(client, &Master);
+				std::string test = "Accepted request";
+
+				send(client, test.c_str(), test.size(), +1);
+			}
+			else {
+
+			}
+		}
+	}
+
+	//closing the socket
+	closesocket(ServerSocket);
+	//cleaning the dll up
+	WSACleanup();
+	return EXIT_SUCCESS;
+}
